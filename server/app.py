2666e6887aa019554470bd8d1d1d3931bf85c032
Nymisha Reddy Cheruku <166729172+nymishareddy@users.noreply.github.com>
2026-03-31 17:51:18 +0530

    Create app.py

---
+++ server/app.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from env import EmailEnv
+
+app = FastAPI()
+
+env = EmailEnv()
+
+@app.get("/")
+def home():
+    return {"message": "Email Env Running"}
+
+@app.post("/reset")
+def reset():
+    obs = env.reset()
+    return {"observation": obs}
+
+@app.post("/step")
+def step(action: str):
+    obs, reward, done, info = env.step(action)
+    return {
+        "observation": obs,
+        "reward": reward,
+        "done": done,
+        "info": info
+    }
+
+@app.get("/state")
+def state():
+    return {"state": env.state()}
